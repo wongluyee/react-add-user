be8b63e04bfe7bb5e7b400fe2c27e862f762be5b
Luyee Wong <wongluyee1015@hotmail.com>
2023-04-09 20:42:52 +0900

    add error modal

---
+++ src/components/Users/AddUser.js
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import styles from "./AddUser.module.css";
+import ErrorModal from "../UI/ErrorModal";
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
+
+  const changeUsernameHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const changeAgeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+
+    if (
+      enteredUsername.trim().length === 0 ||
+      enteredAge.trim().length === 0
+    ) {
+      setError({
+        title: 'Invalid input',
+        message: 'Please enter a valid name and age.'
+      });
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      setError({
+        title: 'Invalid age',
+        message: 'Please enter a valid age (greater than 0).'
+      });
+      return;
+    }
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  return (
+    <div>
+      {error && <ErrorModal title={error.title} message={error.message}/>}
+      <Card className={styles.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={changeUsernameHandler}
+          ></input>
+          <label htmlFor="age">Age (Years)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={changeAgeHandler}
+          ></input>
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
+  );
+};
+
+export default AddUser;
